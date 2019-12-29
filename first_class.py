480c40e6140f703f0bf8e832cd9d11be039bc2d5
santoshbudhathoki <56533899+santoshbudhathoki@users.noreply.github.com>
2019-12-29 09:14:52 +0545

    this is my file sir

---
+++ first_class.py
@@ -0,0 +1,7 @@
+num1=input('enter any number:')
+num2=input('enter any number:')
+
+print(int(num1)+int(num2))
+
+
+
---
+++ hwww.py
@@ -0,0 +1,20 @@
+#subtraction
+num0=input('Enter any number:')
+num1=input('Enter any number:')
+print(int(num0)-int(num1))
+#multiplication
+num2=input('enter any number:')
+num3=input('enter any number:')
+print(int(num2)*int(num3))
+#division
+num=input('Enter any number:')
+num4=input('Enter any number:')
+print(int(num)/int(num4))
+#modulus
+a=input('Enter any number:')
+b=input('Enter any number:')
+print(int(a)%int(b))
+#floor
+c=input('Enter any number:')
+d=input('Enter any number:')
+print(int(c)//int(d))
